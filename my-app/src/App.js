adc90fda5f3ad14eb8e2a5db0f256fe1054e69a9
sunnivaunsgaard <85167045+sunnivaunsgaard@users.noreply.github.com>
2022-02-05 18:22:43 +0100

    add init files

---
+++ my-app/src/App.js
@@ -0,0 +1,21 @@
+import logo from "./logo.svg";
+import "./App.css";
+import ButtonAurora from "./ButtonAurora";
+
+
+function App() {
+  return (
+    <div>
+      <ButtonAurora />
+<div className ="AppSkeleton"> 
+<h1> Weather Forecast</h1>
+
+
+</div>
+
+    </div>
+  );
+     
+}
+
+export default App;
---
+++ my-app/src/ButtonAurora.js
@@ -0,0 +1,12 @@
+import React from "react"; 
+import "./ButtonAurora.css";
+
+
+export default function ButtonAurora () {
+
+  return (
+    <div>
+      <button className="ButtonAurora"> Click here for the Aurora Forecast </button>
+    </div>
+  );
+}
